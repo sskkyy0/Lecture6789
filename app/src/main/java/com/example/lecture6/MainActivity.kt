7f0657dcefd4d4a829ef03de302e385cb9f2d64c
sskkyy0 <zxzxzxzxzxzxzz@naver.com>
2025-02-27 00:45:22 +0900

    feat: 6강 - Scaffold, TextField, Button, 구조분해, SnackBar, 코루틴 스코프

--- app/src/main/java/com/example/lecture6/MainActivity.kt
+++ app/src/main/java/com/example/lecture6/MainActivity.kt
@@ -13,9 +13,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -25,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.lecture6.ui.theme.Lecture6Theme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -33,33 +33,30 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            val (text, setValue)= remember { // 값을 다시 할당하는 setter ???
+            val (text, setValue)= remember {
                 mutableStateOf("")
             }
-            val scaffoldState = rememberBottomSheetScaffoldState()
+            val snackbarHostState = remember { SnackbarHostState() }
             val scope = rememberCoroutineScope()
             val keyboardController = LocalSoftwareKeyboardController.current
-            Scaffold { // 지금 여기서부터 뭔가 잘못됐어
+            Scaffold (
+                snackbarHost = {SnackbarHost(snackbarHostState)}
+            ){
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     TextField(
-                        value = text, // 들어가는 값을 변수형태로 가지고 있어야 입력하는대로 바뀌겟지
-                        onValueChange = { // 이벤트 처리?
-                            setValue
-                        }
+                        value = text,
+                        onValueChange = setValue
                     )
                     Button(onClick = {
                         keyboardController?.hide()
                         scope.launch {
-                            scaffoldState.snackbarHostState.showSnackbar("Hello $text")
+                            snackbarHostState.showSnackbar("Hello $text")
                         }
-                        // suspend 함수...는 코루틴에서 실행을 해야한다
-                        //코루틴이 뭐애?
                     }) {
                         Text("클릭!")
                     }
